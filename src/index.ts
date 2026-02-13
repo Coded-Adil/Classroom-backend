3bda66d7f687d3aaa503042a90cd07ba87be81a5
Coded-Adil <itsadil2022@gmail.com>
2026-02-13 15:50:13 +0600

    Implement Security middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import subjectsRouter from './routes/subjects';
+import securityMiddleware from './middleware/security';
 
 const app = express();
 const PORT = 8000;
 
 // JSON middleware
 app.use(express.json());
+
+app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectsRouter);
 
