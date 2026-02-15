bbca878b752980184f0da574e0a9ae79aa273be8
Coded-Adil <itsadil2022@gmail.com>
2026-02-15 13:21:35 +0600

    implement apm with site24x7

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config()
+
 import express from 'express';
 import cors from 'cors';
 import subjectsRouter from './routes/subjects';
@@ -8,19 +11,21 @@ import { auth } from './lib/auth';
 const app = express();
 const PORT = 8000;
 
-// JSON middleware
-app.use(express.json());
-
-app.use(securityMiddleware);
-
-app.use('/api/subjects', subjectsRouter);
+if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file')
 
 // CORS middleware (must precede route handlers)
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
+
+// JSON middleware
+app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
+
+app.use(securityMiddleware);
 
 app.all('/api/auth/*splat', toNodeHandler(auth));
 
